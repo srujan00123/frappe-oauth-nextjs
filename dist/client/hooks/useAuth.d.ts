5631cf4cea86f8eed38f34ee7e6eb70c1368692c
Srujan N <srujan.00123@gmail.com>
2025-03-27 00:21:19 +0530

    update

--- dist/client/hooks/useAuth.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Hook to access the authentication context
- *
- * @returns Authentication context value
- */
-export declare function useAuth(): import("../context/AuthContext").AuthContextType;
